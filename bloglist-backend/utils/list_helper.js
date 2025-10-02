06e97d542dc865708e0f76384903d6f6a4e0edc5
Lidet Voeun <lidetvoeun@gmail.com>
2025-10-02 14:43:55 +0700

    4.5*: Helper Functions and Unit Tests, step 3

--- bloglist-backend/utils/list_helper.js
+++ bloglist-backend/utils/list_helper.js
@@ -8,7 +8,14 @@ const totalLikes = (blogs) => {
 
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
 };
+const favoriteBlog = (blogs) => {
+  const favBlog = blogs.reduce((fav, blog) => {
+    return blog.likes > fav.likes ? blog : fav;
+  });
+  return favBlog;
+};
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
