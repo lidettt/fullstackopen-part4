66850205e5668ed97ed7907a6dd8f14ac4b8015d
Lidet Voeun <lidetvoeun@gmail.com>
2025-10-13 10:42:36 +0700

    4.9: Blog List Tests, step 2

--- bloglist-backend/tests/blog_list.test.js
+++ bloglist-backend/tests/blog_list.test.js
@@ -5,6 +5,7 @@ const supertest = require("supertest");
 const app = require("../app");
 const helper = require("../tests/test_helper");
 const Blog = require("../models/blog");
+const { request } = require("node:http");
 
 const api = supertest(app);
 
@@ -20,6 +21,12 @@ test("the amount of all the blogs", async () => {
     .expect("Content-Type", /application\/json/);
   const response = await api.get("/api/blogs");
   assert.strictEqual(response.body.length, helper.initialBlogs.length);
+});
+test("verify the unique indentifier of the blog posts to named id", async () => {
+  const response = await api.get("/api/blogs");
+  const blog = response.body[0];
+  assert.ok(blog.id);
+  assert.strictEqual(blog._id, undefined);
 });
 
 after(async () => {
